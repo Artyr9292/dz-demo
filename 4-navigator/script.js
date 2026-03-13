973ad9d03a389cc8888e89a684d0e253dbf83cbd
Artyr <arktik-1992@mail.ru>
2026-03-14 03:51:19 +0500

    5.4. Switch

--- 4-navigator/script.js
+++ 4-navigator/script.js
@@ -1,15 +1,51 @@
-const secretNumber = '7';
+const role = 'ceo';
 
-if(Number(secretNumber) === 7) {
-    console.log('Угадал строго');
+if(role === 'manager') {
+    console.log('Менеджер');
+} else if(role === 'admin') {
+    console.log('Админ');
+} else if(role === 'ceo') {
+    console.log('CEO');
+} else {
+    console.log('Мы тебя не знаем!')
 }
 
-if(secretNumber == 7) {
-    console.log('Угадал не строго');
+switch (role) {
+    case 'manager':
+        console.log('Менеджер');
+        break;
+    case 'admin':
+        console.log('Админ');
+        break;
+    case 'ceo':
+        console.log('CEO');
+        break;
+    default:
+        console.log('Мы тебя не знаем!');
 }
 
-const q = Number(prompt('Введите число'));
-if(q === 7) {
-    console.log(12);
+switch (role) {
+    case 'manager':
+    case 'admin':
+        console.log('Не руководитель');
+        break;
+    case 'ceo' : 
+        console.log('Руководитель');
+        break;
+    default: 
+        console.log('Мы тебя не знаем!');
+}
+
+const num = 1;
+
+switch(true) {
+    case num > 0:
+        console.log('Положительный');
+        break;
+    case num < 0:
+        console.log('Отрицательный');
+        break;
+    default: 
+        console.log('Ноль!');
 }
 
