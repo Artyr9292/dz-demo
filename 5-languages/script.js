9403aac3c3afd9f40aa657fadd9ff9043ed613c8
Artyr <arktik-1992@mail.ru>
2026-03-20 01:43:26 +0500

    5.10. Домашнее задание - Управление потоком

---
+++ 5-languages/script.js
@@ -0,0 +1,15 @@
+let lang = 'lang_en';
+switch (lang) {
+  case "lang_ru":
+    console.log("Включить русский язык");
+    break;
+  case "lang_en":
+    console.log("Включить английский язык");
+    break;
+  case "lang_germany":
+    console.log("Включить немецкий язык");
+    break;
+  default:
+    console.log("Sorry, we are out of " + expr + ".");
+}
+
