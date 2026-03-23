16dffd73f441aaeb6c6c1fe6fb2751afae9258ad
Artyr <arktik-1992@mail.ru>
2026-03-24 02:35:40 +0500

    6.4. Оператор нулевого слияния

--- 5-languages/script.js
+++ 5-languages/script.js
@@ -1,31 +1,4 @@
-//Урок 6.2 Логические операторы
-const isAdmin = true;
-const canWrite = true;
+let age = 0;
 
-console.log(`Системный файл ${isAdmin && canWrite}`);
-console.log(`Обычный файл ${isAdmin || canWrite}`);
-console.log(`Инвертируем права админа ${!isAdmin}`);
-
-const isEdited = true;
-const isSuperAdmin = true;
-console.log(`Системный файл с редактированем ${isAdmin && canWrite && (!isEdited || isSuperAdmin)}`);
-
-
-//Урок 6.3 Операторы с другими типами
-console.log('Вася' || 'Олег');
-console.log(false || 'Олег');
-console.log('Вася' || false);
-console.log(false || false);
-
-console.log('Вася' && 'Олег');
-console.log(false && 'Олег');
-console.log('Вася' && false);
-console.log(false && false);
-
-let a = 'Марина';
-const username = a || 'Петя';
-console.log(username);
-
-const isAdmins = true;
-const filename = isAdmins && 'file.mp4';
-console.log(filename);
+console.log(age || 18);
+console.log(age ?? 18);
