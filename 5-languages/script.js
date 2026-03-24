fcf6bd166a1caa8ec3caf849aa1911cb3a626ea0
Artyr <arktik-1992@mail.ru>
2026-03-25 02:23:00 +0500

    6.4. Оператор нулевого слияния / 6.5. Упражнение - Проверка прав

--- 5-languages/script.js
+++ 5-languages/script.js
@@ -1,4 +1,19 @@
+//6.4. Оператор нулевого слияния
 let age = 0;
 
 console.log(age || 18);
 console.log(age ?? 18);
+
+
+//6.5. Упражнение - Проверка прав
+let balance = 100;
+let bonusBalance = 900;
+let isBanned = false;
+let isExist = false;
+let isSelling = true;
+
+const canBuy = (balance > 1000 || bonusBalance > 100) 
+    && !isBanned
+    && !isExist
+    && isSelling;
+console.log(`Могу купить игру: ${canBuy ? 'Да' : 'Нет'}`);
