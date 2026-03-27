82d0d92d77581b881c25b90644660607e981f39b
Artyr <arktik-1992@mail.ru>
2026-03-28 00:17:52 +0500

    Урок 6.9. Домашнее задание - Булева логика / 2

--- 6-driver/script.js
+++ 6-driver/script.js
@@ -1,7 +1,7 @@
 //Урок 6.9. Домашнее задание - Булева логика
 
 let hasLicence = true;
-let ages = 16;
+let ages = 17;
 let isDrunk = false;
 
 console.log(hasLicence > true || ages > 18 || isDrunk > true);
