5fa3d299a59b0dfc13f8bfb4ee0a1a27300c52ec
Artyr <arktik-1992@mail.ru>
2026-04-18 23:22:36 +0500

    8.8. Деструктуризация

--- 7-converter/script.js
+++ 7-converter/script.js
@@ -1,30 +1,6 @@
-const tasks = ['Задача 1'];
+const userData = ['Антона', 18, 'Москва'];
 
-function Add(task) {
-    tasks.push(task);
-}
+const [userName, _, city] = userData;
 
-function Remove(task) {
-    const index = tasks.indexOf(task);
-    if(index === -1) {
-        return;
-    }
-    return tasks.splice(index, 1);
-}
-
-function Prioritize(task) {
-    const result = Remove(task);
-    if(!result) {
-        return;
-    }
-    tasks.unshift(result[0]);
-}
-
-Add('Задача 2');
-Add('Задача 3');
-console.log(tasks);
-Remove('Задача 2');
-console.log(tasks);
-Prioritize('Задача 3');
-console.log(tasks);
+console.log(userName, city);
 
